cfa91f57eaa0d1cd6ae2bd3efdd29b371c7afc0a
WaffleAtomico <140924351+WaffleAtomico@users.noreply.github.com>
2025-04-03 12:00:13 -0600

    ComponenteBoton y estilos base
    Se agrego el boton de prototipo y la tipografia
    
    Falta agregar iconografia, componente de imput, entre otros

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ButtonIcon from "../components/ButtonIcon";
+
+function Login() {
+    return (
+      <div>
+        <h1>Soy Login: lateef</h1>
+        <h2>Soy titulo</h2>
+        <h3>Subtitulo</h3>
+        <big>Esto es un texto grandote</big>
+        <p>Esto es un texto simple</p>
+        
+      </div>
+    );
+  }
+
+export default Login
